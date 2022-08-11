9457b9eb87c2e8c4cab2af398b8bc3fe636c6bf4
tshenqka <maximatron115@Gmail.com>
2022-08-11 17:13:03 -0400

    Deployed Firebase site. Now working on addingg extra features - Light Mode toggle and also time stamp to text

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
 import './App.css';
+import {Darkmode} from './Darkmode'
 
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/firestore'
-import 'firebase/compat/auth'
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+import 'firebase/compat/analytics';
 
-const auth = firebase.auth();
-const firestore = firebase.firestore();
-
-import { useAuthState } from 'react-firebase-hooks/auth'
-import {useCollectionData } from 'react-firebase-hooks/firestore'
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-  // config
+
+  // your config
   apiKey: "AIzaSyDlHNflPY1yx-Z9fw3ua_llWH_zox1eYHQ",
   authDomain: "simplychat-6310a.firebaseapp.com",
   projectId: "simplychat-6310a",
@@ -21,45 +21,110 @@ firebase.initializeApp({
   appId: "1:509451643319:web:343ae8314fe94424972032"
 })
 
-const SignIn = () => {
-  // sign in method
-  const signInWithGoogle = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider);
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const analytics = firebase.analytics();
 
-    return(
-      <button onClick={signInWithGoogle}>Sign in with Google!</button>
-    )
-    
-  }
-}
-
-const SignOut = () => {
-  return auth.currentUser && (
-    <button onClick={() => auth.signOut()}>Sign Out</button>
-  )
-}
-
-const ChatRoom = () => {
-  const messagesRef = firestore.collection('messages')
-}
-
-const [user] = useAuthState(auth);
 
 function App() {
+
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">
-    
+      <header>
+        <h1>SimplyChat - Made with React + Firebase</h1>
+        <SignOut />
+        <Darkmode />
       </header>
 
       <section>
         {user ? <ChatRoom /> : <SignIn />}
       </section>
+
     </div>
   );
 }
 
+function SignIn() {
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Please be polite or your account will be suspended!</p>
+    </>
+  )
+
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+
+const ChatRoom = () => {
+  const dummy = useRef();
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+  const [messages] = useCollectionData(query, { idField: 'id' });
+  const [formValue, setFormValue] = useState('');
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  return (
+    <div className='homepage'>
+    <main>
+      {messages && messages.map(message => <ChatMessage key={message.id} message={message} />)}
+      <span ref={dummy}></span>
+
+    </main>
+
+    <form onSubmit={sendMessage}>
+
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+
+      <button type="submit" disabled={!formValue}>🕊️</button>
+
+    </form>
+    </div>
+  )
+}
+
+
+function ChatMessage(props) {
+  const { text, uid, photoURL, createdAt } = props.message;
+  console.log(createdAt)
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (<>
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+      <p>{text}</p>
+    </div>
+  </>)
+}
 
 
 export default App;
